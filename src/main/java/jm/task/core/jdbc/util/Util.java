ab33ee3d694ce3961eea38cd084031b1c758b362
Bolotov Alexander <distalker1@gmail.com>
2022-01-17 13:52:50 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DRIVER = "";
+    private static final String HOST = "";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "1234";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(HOST, LOGIN, PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
